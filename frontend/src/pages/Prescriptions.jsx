6f19c52033b4e7af3066f0d534b37ef240e07487
Cheryl Mbani <mbanicheryl45@gmail.com>
2025-10-27 10:32:18 +0300

    feat(Prescriptions): Fetching prescriptions from the API

--- frontend/src/pages/Prescriptions.jsx
+++ frontend/src/pages/Prescriptions.jsx
@@ -1,4 +1,39 @@
+import React from "react";
+import axios from "axios";
+
+
+
+
 function Prescriptions() {
+  const [prescriptions, setPrescriptions] = useState([]);
+  const [filteredPrescriptions, setFilteredPrescriptions] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [searchQuery, setSearchQuery] = useState('');
+
+  // Fetch prescriptions from the API
+  useEffect(()=>{
+    fetchPrescriptions();
+  }, []);
+
+  const fetchPrescriptions=async()=>{
+    try {
+      const response = await axios.get("http://127.0.0.1:5555/api/prescriptions");
+      setPrescriptions(response.data);
+      setFilteredPrescriptions(response.data);
+    } catch(error){
+      console.log("Error fetching prescriptions:", error)
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // Card metrics
+  
+
+  
+
+
+
   return (
     <div>
       <h1>Prescriptions</h1>
